d5155d93a6b0bc4a6c90fdc8a8f5cd1d38ebf0ed
iammshahbaz <shahbaz.ahamed1995@gmail.com>
2024-06-07 02:59:52 +0530

    media

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
     const [loggedIn, setLoggedIn] = useState(false);
@@ -20,21 +21,21 @@ const Navbar = () => {
    
 
     return (
-        <nav className="bg-gray-800 p-4 text-white">
-            <div className="mx-auto flex justify-between items-center">
-                <div>Geekyants</div>
-                <div className="container mx-auto flex align-center justify-end gap-4">
+        <nav className=" bg-gray-800 p-4 text-white">
+            <div className=" navbar-link mx-auto flex justify-between items-center">
+                <div className='text-green-400 text-2xl'>Geekyants</div>
+                <div className=" navbar-link container mx-auto flex align-center justify-end gap-4">
                     <Link to="/dashboard">Dashboard</Link>
                     <Link to="/info">CompanyInfo</Link>
                     <Link to="/">Signup</Link>
                     {location.pathname === '/dashboard' ? (
-                       <button onClick={handleLogout} className="bg-red-500 px-4 py-2 rounded">
+                       <button onClick={handleLogout} className="logout-btn bg-red-500 px-4 py-2 rounded">
                        Logout
                    </button>
                            
                         
                     ) : (
-                         <button onClick={handleLogin} className="bg-green-500 px-4 py-2 rounded">Login</button>
+                         <button onClick={handleLogin} className="login-btn bg-green-500 px-4 py-2 rounded">Login</button>
                     )}
                 </div>
             </div>
